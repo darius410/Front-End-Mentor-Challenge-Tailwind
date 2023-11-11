c1226169a9f10e1141cf5c04a8a9f30381a15e5f
Chaos <tinywonk@yahoo.com>
2023-11-11 13:32:30 -0500

    Changed Files Drastically

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/index.html', // Set the base path for your application
+  
+  build: {
+    outDir: 'dist', // Specify the output directory for the build
+  
+  },
+
+});
